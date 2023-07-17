dc9e71bd0ffb805251f4e551940ae9cdb9386f6a
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-07-17 14:13:17 +0000

    Proyecto final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from 'react';
 
 //include images into your bundle
 
 
 //create your first component
+const TrafficLight = () => {
 
+    const [ color, setColor] = useState("roja");
+    return (
+        <div id="container">
+      <div className={`luz roja ${color === "roja" ? "seleccionada" : ""}`} onClick={() => setColor("roja")}></div>
+      <div className={`luz naranja ${color === "naranja" ? "seleccionada" : ""}`} onClick={() => setColor("naranja")}></div>
+      <div className={`luz verde ${color === "verde" ? "seleccionada" : ""}`} onClick={() => setColor("verde")}></div>
+        </div>
+    )
+};
 
 export default TrafficLight;
