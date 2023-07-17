1228d46fe1499462bebca327ea0491703e7e76a3
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-07-17 15:18:26 +0000

    Proyecto final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,12 +7,24 @@ import React, { useState } from 'react';
 const TrafficLight = () => {
 
     const [ color, setColor] = useState("roja");
+
+    const cambiarColor = () => {
+        if ( color === "roja") setColor("naranja")
+        else if ( color === "naranja") setColor("verde")
+        else setColor("roja")
+    }
+
     return (
+      <>
         <div id="container">
-      <div className={`luz roja ${color === "roja" ? "seleccionada" : ""}`} onClick={() => setColor("roja")}></div>
-      <div className={`luz naranja ${color === "naranja" ? "seleccionada" : ""}`} onClick={() => setColor("naranja")}></div>
-      <div className={`luz verde ${color === "verde" ? "seleccionada" : ""}`} onClick={() => setColor("verde")}></div>
+          <div className={`luz roja ${color === "roja" ? "seleccionada" : ""}`} onClick={() => setColor("roja")}></div>
+          <div className={`luz naranja ${color === "naranja" ? "seleccionada" : ""}`} onClick={() => setColor("naranja")}></div>
+          <div className={`luz verde ${color === "verde" ? "seleccionada" : ""}`} onClick={() => setColor("verde")}></div>
         </div>
+        <div>
+        <button onClick={cambiarColor}> Cambiarcolor</button>
+        </div>
+        </>
     )
 };
 
